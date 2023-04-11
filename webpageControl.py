8413261abb7af6b1b3091a62e20f3c07a9ef98d2
Ningshuo Bai <616353553@qq.com>
2023-04-10 20:35:42 -0400

    add webcontrol

---
+++ webpageControl.py
@@ -0,0 +1,21 @@
+class WebpageControl():
+    def __init__(self) -> None:
+        pass
+
+    def scrollUp():
+        pass
+
+    def scrollDown():
+        pass
+
+    def moveForwar():
+        pass
+
+    def moveBack():
+        pass
+
+    def nextVideo():
+        pass
+
+    def prevVideo():
+        pass
---
+++ youtubeControl.py
@@ -0,0 +1,23 @@
+from webpageControl import WebpageControl
+
+class YoutubeControl(WebpageControl):
+    def __init__(self) -> None:
+        pass
+
+    def scrollUp():
+        pass
+
+    def scrollDown():
+        pass
+
+    def moveForwar():
+        pass
+
+    def moveBack():
+        pass
+
+    def nextVideo():
+        pass
+
+    def prevVideo():
+        pass
