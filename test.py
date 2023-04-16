11933c96945c5a6c70bb72c38f0e144b006822f4
ywugwu <128890731+ywugwu@users.noreply.github.com>
2023-04-15 20:19:32 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from webpageController import WebpageController
 import time
+from gesture import GestureRecognizer
+import cv2
 
 if __name__ == "__main__":
     print("hello")
     controller = WebpageController("https://www.youtube.com")
+    gesture_recognizer = GestureRecognizer()
     while (True):
-        command = input("command:")
+        gesture_recognizer.run()
+        command = gesture_recognizer.get_command()
+        # command = input("command:")
+        print(command)
         if ("w" == command):
             controller.palmMoveDown()
         elif ("s" == command):
@@ -21,3 +27,4 @@ if __name__ == "__main__":
             controller.pointToRight()
         elif ("x" == command):
             controller.makeFist()
+        cv2.waitKey(1)
